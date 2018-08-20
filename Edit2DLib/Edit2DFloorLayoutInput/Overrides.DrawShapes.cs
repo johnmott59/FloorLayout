0443b087ee221e2ace24fd5a1927f6ccde885fae
John Mott <johnkmott@gmail.com>
2018-08-20 08:26:04 -0500

    Add files via upload

---
+++ Edit2DLib/Edit2DFloorLayoutInput/Overrides.DrawShapes.cs
@@ -0,0 +1,31 @@
+﻿#define DOTNET
+using System.Collections.Generic;
+using ShapeTemplateLib;
+using ShapeTemplateLib.Templates.User0;
+
+namespace Edit2DLib
+{
+    public partial class Edit2DFloorLayoutInput
+    {
+        public override void DrawShapes()
+        {
+            this.Clear("#FFFFFF");
+#if DOTNET
+            // The model for .net is to invalidate and let the paint handler draw.
+            if (oGraphics == null && DrawPrimitives == null) return;
+
+
+#else
+#endif
+            // Draw the outline and the open area
+
+            this.OutlineAreas.DrawShapes();
+            this.OpenAreas.DrawShapes();
+            this.Walls.DrawShapes();
+
+            DrawGrid();
+            DrawAxis();
+
+        }
+    }
+}
---
+++ Edit2DLib/Edit2DFloorLayoutInput/Overrides.Mouse.cs
@@ -0,0 +1,71 @@
+﻿namespace Edit2DLib
+{
+    public partial class Edit2DFloorLayoutInput
+    {
+        /*
+         * Sub classes will override these methods to implement mouse operations on their canvas
+         */
+       override  public eMouseDownCapture MouseDown(int ScreenMouseX, int ScreenMouseY)
+        {
+            /*
+             * Try to capture something in the outline, then the open area, in turn
+             */
+            if (OutlineAreas.TryHoleSelect(ScreenMouseX, ScreenMouseY)) return eMouseDownCapture.VertexHandle;
+
+            if (OpenAreas.TryHoleSelect(ScreenMouseX, ScreenMouseY)) return eMouseDownCapture.VertexHandle;
+
+            eMouseDownCapture sts = Walls.MouseDown(ScreenMouseX, ScreenMouseY);
+
+            return sts;
+
+        }
+
+        override public bool MouseMove(eMouseState MouseState, int ScreenMouseX, int ScreenMouseY, int ScreenDeltaX, int ScreenDeltaY)
+        {
+            /*
+             * See if we are moving anything in the outline, then the open area
+             */
+            if (OutlineAreas.MouseMove(MouseState, ScreenMouseX, ScreenMouseY, ScreenDeltaX, ScreenDeltaY))
+            {
+                return true;
+            }
+
+            if (OpenAreas.MouseMove(MouseState, ScreenMouseX, ScreenMouseY, ScreenDeltaX, ScreenDeltaY))
+            {
+                return true;
+            }
+
+            if (Walls.MouseMove(MouseState,ScreenMouseX,ScreenMouseY,ScreenDeltaX,ScreenDeltaY))
+            {
+                return true;
+            }
+
+            /*
+             * If we are here we scrolled the background. Update the dependent controls
+             */
+            int WorldDeltaX = (int)((float)ScreenDeltaX * CurrentZoom);
+            int WorldDeltaY = (int)((float)ScreenDeltaY * CurrentZoom);
+
+            OutlineAreas.WorldPointScrollX -= WorldDeltaX;
+            OutlineAreas.WorldPointScrollY -= WorldDeltaY;
+
+            OpenAreas.WorldPointScrollX -= WorldDeltaX;
+            OpenAreas.WorldPointScrollY -= WorldDeltaY;
+
+            Walls.WorldPointScrollX -= WorldDeltaX;
+            Walls.WorldPointScrollY -= WorldDeltaY;
+
+            return false;
+        }
+
+        override public void MouseUp(int ScreenMouseX, int ScreenMouseY)
+        {
+            /*
+             * Reset the currently selected items. Most recently selected remain active for any commands
+             */
+            OutlineAreas.MouseUp(ScreenMouseX, ScreenMouseY);
+            OpenAreas.MouseUp(ScreenMouseX, ScreenMouseY);
+            Walls.MouseUp(ScreenMouseX, ScreenMouseY);
+        }
+    }
+}
